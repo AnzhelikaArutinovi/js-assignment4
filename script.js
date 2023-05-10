4ad2ac5cfeaa663eb395f21f9c20eeefe58f3020
AnzhelikaArutinovi <anzhelika.arutinovi.1@btu.edu.ge>
2023-05-11 02:11:50 +0400

    To do list - started

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 "use strict";
 
-// let inputForm = document.getElementById("input-form");
-// let ulElement = document.getElementById("ul-list");
-// let btnClear = document.getElementById("btn-clear");
-// let formElement = document.getElementById("form-wraper");
+let inputForm = document.getElementById("input-form");
+let ulElement = document.getElementById("ul-list");
+let btnClear = document.getElementById("btn-clear");
+let formElement = document.getElementById("form-wraper");
 
-// formElement.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   let inputValue = inputForm.value;
-//   let liElement = document.createElement("li");
-//   let btnDelete = document.createElement("button");
+formElement.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let inputValue = inputForm.value;
+  let liElement = document.createElement("li");
+  let btnDelete = document.createElement("button");
+  let btnCheck = document.createElement("button");
 
-//   btnDelete.addEventListener("click", function () {
-//     liElement.remove();
-//   });
-//   liElement.innerText = inputValue;
-//   liElement.appendChild(btnDelete);
-//   ulElement.appendChild(liElement);
-//   inputForm.value = " ";
-// });
+  btnDelete.addEventListener("click", function () {
+    liElement.remove();
+  });
+  btnCheck.addEventListener("click", function () {
+    liElement.remove();
+  });
+  liElement.appendChild(btnCheck);
+  liElement.innerText = inputValue;
+  btnDelete.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+  btnDelete.style.background = "transparent";
+  liElement.appendChild(btnDelete);
+  ulElement.appendChild(liElement);
+  inputForm.value = " ";
+});
 
-// btnClear.addEventListener("click", function () {
-//   ulElement.innerHTML = " ";
-// });
+btnClear.addEventListener("click", function () {
+  ulElement.innerHTML = " ";
+});
 
-// inputForm.addEventListener("focus", function () {
-//   this.style.border = "none";
-// });
+inputForm.addEventListener("focus", function () {
+  inputForm.style.outline = "none";
+});
 
+// დავალება 1
 // 1. შექმენით მასივი, რომლის საწყისი მნიშვნელობა იქნება რიცხვები: 5, 25, 89, 120, 36.
 // let array1 = [5, 25, 89, 120, 36];
 
@@ -45,10 +53,16 @@
 // array2.push("ვაშლი", "ანანასი");
 // array2.unshift("საზამთრო");
 // console.log(array2.length);
-// array2.splice(2, "", "მანგო");
+// array2.splice(2, " ", "მანგო");
 // array2.shift();
 // array2.pop();
-// console.log(array2.length);
+// console.log(array2);
+
+// 3. Splice მეთოდის საშუალებით მიიღეთ: [1, 2, 3, 'a', 'b', 'c', 4, 5].
+// let array3 = [1, 2, 3, 4, 5];
+
+// array3.splice(3, " ", "a", "b", "c");
+// console.log(array3);
 
 // 4. გაფილტრეთ და გამოიტანეთ მარტო ის სიტყვები რომლების სიმბოლოების რაოდენობა იქნება 3-ზე მეტი.
 // let languages = ["html", "css", "javascript", "python", "php"];
@@ -69,6 +83,3 @@
 
 // array6.splice(2, 1);
 // console.log(array6);
-
-
-// 3 და 6 არის გასაკეთებელი + დავალება
